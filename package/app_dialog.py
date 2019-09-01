ec190a6d3f4127df244c82709dfe42c39474ca6c
Linus Behrens <linus@thebehrens.net>
2019-09-01 18:36:22 +0200

    created extra file for dialog.ui setup

---
+++ package/app_dialog.py
@@ -0,0 +1,31 @@
+from PyQt5 import QtWidgets
+import sys
+from package.util.slash import slash
+from package.ui.window import Dialog
+
+class App_Dialog():
+    def __init__(self):
+        app = QtWidgets.QApplication(sys.argv)
+        self.dialog = Dialog()
+        accept_username_password = self.dialog.findChild(QtWidgets.QPushButton, 'pushButton_ok')
+        accept_username_password.clicked.connect(self.dialog_ok)
+        accept_username_password = self.dialog.findChild(QtWidgets.QPushButton, 'pushButton_cancel')
+        accept_username_password.clicked.connect(self.dialog_cancel)
+        self.username_line = self.dialog.findChild(QtWidgets.QLineEdit, 'lineEdit_username')
+        self.password_line = self.dialog.findChild(QtWidgets.QLineEdit, 'lineEdit_password')
+        res = app.exec_()
+        del app
+        del self.dialog
+        sys.exit(res)
+
+    def dialog_cancel(self):
+        self.dialog.close()
+        #self.dialog = None
+
+    def dialog_ok(self):
+        username_line_content = self.username_line.text()
+        password_line_content = self.password_line.text()
+        print(username_line_content)
+        print(password_line_content)
+        self.dialog.close()
+        #self.dialog = None
